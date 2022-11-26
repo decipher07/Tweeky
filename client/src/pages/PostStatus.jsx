7b0985322d0c04bb260ee412c74cd709b79543a9
decipher07 <social.deej@gmail.com>
2022-11-27 00:39:56 +0530

    🍱 feat: adding post status page

---
+++ client/src/pages/PostStatus.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useState } from 'react'
+import './PostStatus.css'
+import axios from 'axios';
+
+function PostStatus() {
+    const [message, setMessage] = useState("");
+
+    const handleSubmit = async (e) => {
+
+        const headers = {
+            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MzgxYjFkMmY2YTU0ODVlMWZjNGJjYTkiLCJlbWFpbCI6InNvbWV0aGluZzEyQGdtYWlsLmNvbSIsImlhdCI6MTY2OTQ0NDczOSwiZXhwIjoxNjcyMDM2NzM5fQ.hgNBInTeeSp2n-ZmiGBKmmPVdrnKwbCZ0dvJerN_WhM',
+        };
+
+        const body = { content: message };
+        const response = await axios.post('http://localhost:3001/posts/post', body, { headers });
+        console.log(response);
+
+        if ( response.data.success )
+            window.location = window.location
+    }
+
+    return (
+        <div className="main">
+            <div className="postFrameForUsers">
+                <div className='headers'>
+                    <h1>You can Post a status here</h1>
+                </div>
+
+                <div className="statusCode">
+                    <form action="/poststatus" method="get" onSubmit={handleSubmit}>
+                        <textarea className='transparent-message' name="message" id="message" cols="30" rows="10" value={message} onChange={(e) => setMessage(e.target.value)} >
+                        </textarea>
+                        <div>
+                            <input type="submit" className="btn btn-dark submit" value="Submit" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PostStatus
