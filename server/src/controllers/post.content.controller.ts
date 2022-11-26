6a9236f437cd4f1c2ff616e7d8d00b28a998ba42
decipher07 <social.deej@gmail.com>
2022-11-26 13:05:19 +0530

    💚 feat: getting feeds for user

--- server/src/controllers/post.content.controller.ts
+++ server/src/controllers/post.content.controller.ts
@@ -2,6 +2,7 @@ import { Response, NextFunction } from "express";
 import Logging from "../logger/logging";
 import { UserIdRequest } from "../types/app-request";
 import Post, { IPostModel } from "../models/Post";
+import Followers from "../models/Followers";
 
 /** Create a Post */
 const createPostController = async ( req: UserIdRequest, res: Response, next: NextFunction ) : Promise <Response> => {
@@ -83,7 +84,42 @@ const getAParticularPost = async ( req: UserIdRequest, res: Response, next: Next
 
 /** Read a Post 
  *  Both from Current Users as well as the Users followed
- * 
  */
+const getPostsFromAllTheFollowings = async ( req: UserIdRequest, res: Response, next: NextFunction ) : Promise <Response> => {
 
-export { createPostController, updatePostController, deletePostController, getAllPostsByAUser, getAParticularPost };
+    const { postId } = req.query ;
+    
+    try {
+        /** 
+         * Populate the Following of the User 
+         * Search up for their posts 
+         * Output the result 
+         */
+
+        const getAllFollowingsOfTheUser = (await Followers.findOne({"userId": req.userDocument.userId}).populate('following', '_id username name'))!;
+        
+        let finalArrayOfUsers = [];
+        for ( let followers of getAllFollowingsOfTheUser!.following ){
+            // Lookup for the posts by the given user 
+            // @ts-ignore
+            const postFromTheCurrentIterativeUser = await Post.find({ "userId" : followers._id }).select({ "content" : 1, "_id": 0 })
+            
+            // Creating the sample response
+            let templateForStoringResponse = {
+                name: followers.name,
+                username: followers?.username,
+                posts: postFromTheCurrentIterativeUser
+            }
+
+            // Pushing the aggregate
+            finalArrayOfUsers.push(templateForStoringResponse);
+        }
+        
+        return res.status(200).json({"success": true, "data": finalArrayOfUsers, "message": null});
+    } catch ( err: any ){
+        Logging.error(err.message);
+        return res.status(500).json({"success": false, "data": null, "message": "Something went wrong"});
+    }
+}
+
+export { createPostController, updatePostController, deletePostController, getAllPostsByAUser, getAParticularPost, getPostsFromAllTheFollowings };
